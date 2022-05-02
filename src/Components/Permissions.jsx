110c6ac3073724710001641b5a910b131d4ecabe
Divya <divya14402@gmail.com>
2022-05-02 13:50:28 +0530

    permissions

---
+++ src/Components/Permissions.jsx
@@ -0,0 +1,28 @@
+import { Box, Typography } from '@material-ui/core';
+// import { useEffect } from 'react';
+//import departments from '../Database/db';
+
+const Permissions = () => {
+  return (
+    <Box
+      component="main"
+      sx={{ flexGrow: 0, bgcolor: 'red', p: 6, height: '100vh' }}
+    >
+      <Typography paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
+        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
+        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
+        tellus. Convallis convallis tellus id interdum velit laoreet id donec
+        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
+        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
+        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
+        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
+        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
+        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
+        Lorem donec massa sapien faucibus et molestie ac.
+      </Typography>
+    </Box>
+  );
+};
+export default Permissions;
